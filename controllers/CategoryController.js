041f227d68039d0e4c718e75fbc88a070be6d1a0
mrofiqfdilah <mrofiqfadil@gmail.com>
2025-01-09 09:45:11 +0700

    feat(category): add update category endpoint

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -48,8 +48,28 @@ const ShowCategory = async (req, res) => {
     }
 }
 
+const UpdateCategory = async (req, res) => {
+    try {
+        const Data = await Category.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        return res.status(200).json({
+            status: 'success',
+            data: Data
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: error.message
+        })
+    }
+}
+
+
 export {
     CreateCategory,
     AllCategory,
-    ShowCategory
+    ShowCategory,
+    UpdateCategory
 }
--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { AllCategory, CreateCategory, ShowCategory } from "../controllers/CategoryController.js";
+import { AllCategory, CreateCategory, ShowCategory, UpdateCategory } from "../controllers/CategoryController.js";
 const router = express.Router();
 
 router.post('/category', CreateCategory)
 router.get('/category', AllCategory)
 router.get('/category/:id', ShowCategory);
+router.put('/category/:id', UpdateCategory);
 
 export default router;
