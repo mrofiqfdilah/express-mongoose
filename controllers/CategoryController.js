1b4f5117f94e8be32d8f0e26f2e8bc526d7da6ee
mrofiqfdilah <mrofiqfadil@gmail.com>
2025-01-09 09:52:18 +0700

    feat(category): add destroy category endpoint

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -66,10 +66,34 @@ const UpdateCategory = async (req, res) => {
     }
 }
 
+const DestroyCategory = async (req, res) => {
+    try {
+        const Data = await Category.findByIdAndDelete(req.params.id);
+
+        if (!Data) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'Category not found'
+            });
+        }
+
+        return res.status(200).json({
+            status: 'success',
+            message: 'Category deleted successfully'
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: error.message
+        });
+    }
+};
 
 export {
     CreateCategory,
     AllCategory,
     ShowCategory,
-    UpdateCategory
-}
+    UpdateCategory,
+    DestroyCategory
+};
+
--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { AllCategory, CreateCategory, ShowCategory, UpdateCategory } from "../controllers/CategoryController.js";
+import { AllCategory, CreateCategory, DestroyCategory, ShowCategory, UpdateCategory } from "../controllers/CategoryController.js";
 const router = express.Router();
 
 router.post('/category', CreateCategory)
 router.get('/category', AllCategory)
 router.get('/category/:id', ShowCategory);
 router.put('/category/:id', UpdateCategory);
+router.delete('/category/:id', DestroyCategory);
 
 export default router;
